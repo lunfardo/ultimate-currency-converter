6ace5537df830d638897023da249c652c52c35a1
lunfalun <lunfalun@DESKTOP-M239PTC>
2021-06-01 19:30:00 +0200

    Operation Icon change

--- src/components/atoms/OperationModeArrowIcon.tsx
+++ src/components/atoms/OperationModeArrowIcon.tsx
@@ -1,18 +1,37 @@
-import { IconButton } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core";
 import { ArrowBack, ArrowForward } from "@material-ui/icons";
 import { OperationMode } from "../../types";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(1.5),
+  },
+  withError: {
+    color: "red",
+  },
+  NoError: {
+    color: "green",
+  },
+}));
 
 export const OperationModeArrowIcon: React.FC<{
   operationMode: OperationMode;
   error: Boolean;
 }> = ({ operationMode, error }) => {
+  const classes = useStyles(error);
   return (
-    <IconButton style={{ color: error ? "red" : "green" }}>
+    <div
+      className={clsx(
+        classes.root,
+        error ? classes.withError : classes.NoError
+      )}
+    >
       {operationMode === OperationMode.Forward ? (
         <ArrowForward />
       ) : (
         <ArrowBack />
       )}
-    </IconButton>
+    </div>
   );
 };
